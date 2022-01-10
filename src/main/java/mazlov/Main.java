6bbb817c77889779ff0306ddc0c82d99d2f60f37
Maksym Mazlov <mazlovmaxim@gmail.com>
2022-01-10 14:05:51 +0200

    update logs

--- src/main/java/mazlov/Main.java
+++ src/main/java/mazlov/Main.java
@@ -10,22 +10,37 @@ import mazlov.service.CouponService;
 import mazlov.service.CouponServiceImpl;
 import mazlov.service.TicketService;
 import mazlov.service.TicketServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger log = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         TicketService ticketService = new TicketServiceImpl();
         CacheServiceImpl cache = new CacheServiceImpl();
         CouponService couponService = new CouponServiceImpl(cache);
         BaggageService baggageService = new BaggageServiceImpl();
 
-        System.out.println(ticketService.checkTicketById(2L));
-        System.out.println();
-        System.out.println(StorageCoupon.all());
-        System.out.println(StorageTicket.all());
-        System.out.println(couponService.getPriceByCouponId(57, 100));
 
-        baggageService.addBaggage(new Baggage(1, 1));
-        baggageService.addBaggage(new Baggage(2, 2));
-        System.out.println(baggageService.addBaggage(new Baggage(2, 2)));
+        log.info("--------- Tickets test --------");
+        log.info(StorageTicket.all());
+        log.info(ticketService.checkTicketById(2L));
+
+
+        log.info("--------- Coupon test --------");
+        log.info(StorageCoupon.all());
+        log.info(couponService.getPriceByCouponId(57, 100));
+
+
+        log.info("--------- Baggage test --------");
+        Baggage baggage = new Baggage(1, 1);
+        log.info("Adding baggage: {}, result: {}", baggage, baggageService.addBaggage(baggage));
+
+        baggage = new Baggage(2, 2);
+        log.info("Adding baggage: {}, result: {}", baggage, baggageService.addBaggage(baggage));
+
+        baggage = new Baggage(2, 2);
+        log.info("Adding baggage: {}, result: {}", baggage, baggageService.addBaggage(baggage));
     }
 }
